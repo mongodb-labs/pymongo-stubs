dcc998b8bef2c1620ef6457efe890727ad4d41bf
Shane Harvey <shnhrv@gmail.com>
2020-11-17 16:36:33 -0800

    PYTHON-2427 Use Any as return type for documents (#7)
    Add test suite which is also checked via mypy --strict.

---
+++ test/test_pymongo.py
@@ -0,0 +1,54 @@
+# Copyright 2020-present MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test type checking of pymongo."""
+
+import unittest
+
+from pymongo.mongo_client import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+
+class TestPymongo(unittest.TestCase):
+    client: MongoClient
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.client = MongoClient(
+            serverSelectionTimeoutMS=250, directConnection=False)
+        try:
+            cls.client.admin.command('ping')
+        except ServerSelectionTimeoutError as exc:
+            raise unittest.SkipTest(f'Could not connect to MongoDB: {exc}')
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.client.close()
+
+    def test_insert_find(self) -> None:
+        doc = {'my': 'doc'}
+        coll = self.client.test.test
+        coll2 = self.client.test.test2
+        result = coll.insert_one(doc)
+        self.assertEqual(result.inserted_id, doc['_id'])
+        retreived = coll.find_one({'_id': doc['_id']})
+        if retreived:
+            # Documents returned from find are mutable.
+            retreived['new_field'] = 1
+            result2 = coll2.insert_one(retreived)
+            self.assertEqual(result2.inserted_id, result.inserted_id)
+
+
+if __name__ == "__main__":
+    unittest.main()
